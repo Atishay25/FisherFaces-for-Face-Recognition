f1e298f58f1f89db2be1d6f11af7b333d562133c
Atishay25 <atishayjain2552@gmail.com>
2023-11-27 23:09:33 +0530

    loading data and algos

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 
 class PCA:
@@ -15,4 +16,94 @@ class PCA:
         for i in range(V.shape[1]):
             V[:, i] = V[:, i]/np.linalg.norm(V[:, i])
         V_k = V[:, :self.k]
+
+class Linear_discriminant_Analysis:
+    def __init__(self, out_dim=1):
+        super(Linear_discriminant_Analysis, self).__init__()
+        self.out_dim = out_dim
+        self.projected_faces = None     # projection of images on eigenspace
+        self.mean_face = None           # mean of images of all classes
+        self.W = None                   # eigenspace
+
+        def fit(self, X, labels):       # X: nxd
+            self.n = X.shape[0]
+            self.d = X.shape[1]
+
+            unique_labels = np.unique(labels)
+            num_classes = len(unique_labels)    # number of unique lables(classes)
+
+            # Compute mean face for each class
+            class_mean_faces = []
+            for label in unique_labels:
+                class_images = X[labels == label]
+                class_mean_face = np.mean(class_images, axis=0)
+                class_mean_faces.append(class_mean_face.reshape(-1,1))
+
+            self.mean_face = np.mean(class_mean_faces, axis=0)  # mean of images of all classes
+
+            # Compute scatter matrices
+            Sw = np.zeros((self.d, self.d))
+            Sb = np.zeros((self.d, self.d))
+
+            for label, class_mean_face in zip(unique_labels, class_mean_faces):
+                class_images = X[labels == label]
+
+                diff_within = class_images - class_mean_face
+                Sw += (diff_within.T@diff_within)
+
+                diff_between = class_mean_face - self.mean_face
+                Sb += len(class_images) * (diff_between@diff_between.T)
+
+            # Solve the generalized eigenvalue problem
+            eigenvalues, self.W = np.linalg.eigh(np.linalg.inv(Sw).dot(Sb))     # W is eigenvector matrix
+
+            # Sort eigenvalues and corresponding eigenvectors in descending order
+            indices = np.argsort(eigenvalues)[::-1]
+            self.all_eigenvalues = eigenvalues[indices]
+            self.all_eigenvectors = self.W[:, indices]
+
+            # Select the top eigenfaces
+            indices = indices[:self.out_dim]
+            eigenvalues = eigenvalues[indices]
+            self.W = self.W[:, indices]
+
+            # Project faces onto the subspace spanned by the top eigenfaces
+            self.projected_faces = np.dot(X - self.mean_face, self.W)
+
+
+
+class Fisherfaces:
+    def __init__(self, num_components=1):
+        super(Fisherfaces, self).__init__()
+        self.num_components = num_components
+        self.fisher_ld = None
+        self.labels = []
+        self.projected_image = None
+
+    def fit(self, X, labels):
+        self.n = X.shape[0]
+        self.d = X.shape[1]
+        self.c = labels.max()+1
+        
+        self.fisher_ld = Linear_discriminant_Analysis(out_dim=self.num_components)
+        ##############  here add code for self.pca = PCA() ###############
+        #self.pca = PCA(k = self.n - self.c)
+        ##############  here add code to change X according to PCA algorithm ###############
+        #modified_X = self.pca.fit_transform(X)
+        self.fisher_ld.fit(X, labels)
+        self.labels = labels
+
+    def predict(self, image):
+        # Project the input image onto the subspace
+        ##############  here add code to change "image" to be transformed according to PCA algorithm ###############
+        # image = self.pca.transform(X)
+        projected_image = np.dot(image - self.fisher_ld.mean_face, self.fisher_ld.W)
+
+        # Find the nearest neighbor in the subspace
+        distances = [np.linalg.norm(projected_image - face) for face in self.fisher_ld.projected_faces]
+        min_distance_index = np.argmin(distances)
+
+        return self.labels[min_distance_index]
+    
+
     
--- src/load_data.py
+++ src/load_data.py
@@ -1,16 +1,102 @@
 import os
 from PIL import Image
-from torch.utils.data import DataLoader, random_split
-from torchvision import transforms, datasets
-from torch.utils.data import Dataset
+import numpy as np
+import matplotlib.pyplot as plt
 
 class YaleDataset(object):
     def __init__(self, data_path):
+        super(YaleDataset, self).__init__()
         self.data_path = data_path
-        self.data = self.load_data()
         self.n = 165
+        self.X_train = np.zeros((105,77760))
+        self.y_train = np.zeros((105,1), dtype=np.int32)
+        self.X_test = np.zeros((60,77760))
+        self.y_test = np.zeros((60,1), dtype=np.int32)
+        self.X_glasses = np.zeros((30,77760))
+        self.y_glasses = np.zeros((30,1), dtype=np.int32)
+        
+    def load_data(self):
+        train_indices = [None]*15
+        img_read_per_person = [0]*15
+        num_train = 0
+        num_test = 0
+        glass_pids = []
+        num_glasses = 0
+        for i in range(15):     # 11 images split into 7 for train and 4 for test
+            train_indices[i] = list(np.random.choice(11, 7, replace=False))
+        for f in os.listdir(self.data_path):
+            if not 'txt' in f and not 'DS_Store' in f and not 'zip' in f:
+                img_path = os.path.join(self.data_path, f)
+                image = plt.imread(img_path)
+                pid = int(f.split('.')[0].split('subject')[1]) - 1
+                if img_read_per_person[pid] in train_indices[pid]:
+                    self.X_train[num_train,:] = image.flatten()
+                    self.y_train[num_train] = pid
+                    num_train += 1
+                else:
+                    self.X_test[num_test,:] = image.flatten()
+                    self.y_test[num_test] = pid
+                    num_test += 1
+                img_read_per_person[pid] += 1
+                if 'glasses' in f:
+                    self.X_glasses[pid,:] = image.flatten()
+                    self.y_glasses[pid] = 1
+                    num_glasses += 1
+                    glass_pids.append(pid)
+                elif 'noglasses' in f:
+                    self.X_glasses[pid,:] = image.flatten()
+                    self.y_glasses[pid] = 0
+                    num_glasses += 1
+                    glass_pids.append(pid)
+        # print(img_read_per_person, num_train, num_test) #[11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11] 105 60
+        # here they did argsort, Idk why
 
-def load_data(data_path, save_path):
+class YaleB(object):
+    def __init__(self, data_path):
+        super(YaleB, self).__init__()
+        self.data_path = data_path
+        self.n = 2470
+        self.X_train = np.zeros((1482,32256))
+        self.y_train = np.zeros((1482), dtype=np.int32)
+        self.X_test = np.zeros((988,32256))
+        self.y_test = np.zeros((988), dtype=np.int32)
+    def load_data(self):
+        train_indices = [None]*38
+        img_read_per_person = [0]*38
+        num_train = 0
+        num_test = 0
+        for i in range(38):     # 64 images split into 39 for train and 26 for test
+            train_indices[i] = list(np.random.choice(65, 39, replace=False))
+        for p_dir in os.listdir(self.data_path):
+            p_path = os.path.join(self.data_path, p_dir)
+            for f in os.listdir(p_path):
+                img_path = os.path.join(p_path, f)
+                image = plt.imread(img_path)
+                pid = int(p_dir[5:]) - 1
+                if pid > 13:
+                    pid -= 1
+                if img_read_per_person[pid] in train_indices[pid]:
+                    self.X_train[num_train,:] = image.flatten()
+                    self.y_train[num_train] = pid
+                    num_train += 1
+                else:
+                    self.X_test[num_test,:] = image.flatten()
+                    self.y_test[num_test] = pid
+                    num_test += 1
+                img_read_per_person[pid] += 1
+        #print(img_read_per_person, num_train, num_test)
+        #print(img_read_per_person, self.X_train.shape[0], self.X_test.shape[0])
+        # sort and take nonzero indices
+        #idx = np.argsort(self.y_train)
+        #self.X_train = self.X_train[idx,:]
+        #self.y_train = self.y_train[idx]
+#
+        #idx = np.argsort(self.y_test)
+        #self.X_test = self.X_test[idx,:]
+        #self.y_test = self.y_test[idx]
+        #print(img_read_per_person, self.X_train.shape[0], self.X_test.shape[0])
+
+def ld(data_path, save_path):
     if not os.path.isdir(save_path):
         os.mkdir(save_path)
 
@@ -24,13 +110,10 @@ def load_data(data_path, save_path):
             if not os.path.isfile(img_path):
                 image.save(img_path)
 
-def preprocessing():
 
-    train_dataset, test_dataset  = random_split(data, [130,35])
+if __name__ == '__main__':
+    #yale = YaleDataset('./../data/yale')
+    #yale.load_data()
 
-    batch_size = 16
-
-    yaledata = {'train_load' : DataLoader(train_dataset, batch_size = batch_size, shuffle=False),
-                'test_load' : DataLoader(test_dataset, batch_size = batch_size)}
-
-    return yaledata
+    yale_B = YaleB('./../data/yaleB')
+    yale_B.load_data()
